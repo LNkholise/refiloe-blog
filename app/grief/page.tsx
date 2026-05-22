ae83faf0b290c8135798a7d0aa0ed2d66790e9b3
Leonard Nkholise <98850226+LNkholise@users.noreply.github.com>
2026-05-22 16:54:06 +0200

    Update Hero component text for Grief section

--- app/grief/page.tsx
+++ app/grief/page.tsx
@@ -9,9 +9,9 @@ export default function ComingSoon() {
     <div>
       <Hero 
         menu={<Navbar direction="down" />} 
-        headingSerif="Grief"
-        headingSans="Section"
-        introText="When death visited and revisited me, making itself my unwanted business, it revealed a need many do not recognize until bereavement-related grief arrives. Since the age of 11, I have carried grief that I now know is cumulative. Realizing both society’s limited understanding of this kind of grief and the need for grievers to be held in ways that suit them, I set out to explore how to answer that question."
+        headingSerif="On"
+        headingSans="Grief"
+        introText="When death visited and revisited me, making itself my unwanted business, it revealed a need many do not recognize until bereavement-related grief arrives. Since the age of 11, I have carried grief that I now know is cumulative. Realizing both the gaps in understanding of this kind of grief and the need for grievers to be supported in ways that suit them, I set out to explore some of the answers."
         bgImageUrl="https://images.pexels.com/photos/15583002/pexels-photo-15583002.jpeg"
       />
       <ComingSoonBody>
