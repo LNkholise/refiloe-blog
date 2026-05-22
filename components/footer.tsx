011b4373f9506b25786e72862415859d985d967d
Leonard Nkholise <98850226+LNkholise@users.noreply.github.com>
2026-05-22 17:44:46 +0200

    Update footer.tsx

--- components/footer.tsx
+++ components/footer.tsx
@@ -22,7 +22,7 @@ const Footer: React.FC = () => {
         { name: "Home", url: "/" },
         { name: "The Regathering", url: "/blog" },
         { name: "Grief", url: "/grief" },
-        { name: "Collabs, Collaborations, Connections", url: "/projects" },
+        { name: "Curations, Collaborations, Connections", url: "/projects" },
       ],
     },
     {
