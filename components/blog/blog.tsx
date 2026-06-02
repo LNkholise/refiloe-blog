581ec6c90afcc00614b80d835408604ea1155389
Leonard Nkholise <98850226+LNkholise@users.noreply.github.com>
2026-06-02 12:00:13 +0200

    Refine blog description for clarity

--- components/blog/blog.tsx
+++ components/blog/blog.tsx
@@ -40,7 +40,7 @@ export default function WriterBlog({ menu, mainArticles, oldArticles }: { menu: 
         </div>
         <div className="lg:w-1/4">
           <p className="text-sm leading-relaxed text-muted-foreground tracking-widest font-sans">
-            This is not just a blog. This is a hard-won return to exploring the extraordinary, the everyday, and everything in between.
+            This is not a blog. This is a hard-won return to openly exploring the extraordinary, the everyday, and everything in between.   
           </p>
         </div>
       </div>
