d76adcfdac041cfe38f15a83192450d8e335a8b8
Leonard Nkholise <98850226+LNkholise@users.noreply.github.com>
2026-06-02 13:17:56 +0200

    Rename LetterSection to LettersSection in grief page

--- app/grief/page.tsx
+++ app/grief/page.tsx
@@ -15,7 +15,7 @@ export default function ComingSoon() {
         introText="When death visited and revisited me, making itself my unwanted business, it revealed a need many do not recognize until bereavement-related grief arrives. Since the age of 11, I have carried grief that I now know is cumulative. Realizing both the gaps in understanding of this kind of grief and the need for grievers to be supported in ways that suit them, I set out to explore some of the answers."
         bgImageUrl="https://images.pexels.com/photos/15583002/pexels-photo-15583002.jpeg"
       />
-      <LetterSection />
+      <LettersSection />
       <ComingSoonBody>
         <div className="space-y-8 flex flex-col items-center">
           <p className="max-w-2xl text-base md:text-lg leading-relaxed text-primary font-sans">
