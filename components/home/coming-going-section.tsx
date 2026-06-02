a6ec1ab65019be3de85090eca8a2961544b0e966
Leonard Nkholise <98850226+LNkholise@users.noreply.github.com>
2026-06-02 13:40:15 +0200

    Fix positioning of bottom div in coming-going-section
    Adjusted positioning of the bottom div for better layout.

--- components/home/coming-going-section.tsx
+++ components/home/coming-going-section.tsx
@@ -16,11 +16,9 @@ export default function ComingGoingSection() {
          </p>
         </div>
       </div>
-      
-      <div className="relative z-0 flex w-full lg:w-[25%] bg-background min-h-[10vh] lg:min-h-screen border-r border-muted/20">
       </div>
       
-      <div className="absolute z-20 bottom-20 left-10 lg:bottom-20 lg:left-[5%] lg:right-auto w-[60%] lg:w-[35%] h-[30vh] lg:h-[80%] animate-in fade-in duration-1000 delay-300">
+      <div className="absolute z-20 bottom-25 left-10 lg:bottom-20 lg:left-[5%] lg:right-auto w-[60%] lg:w-[35%] h-[30vh] lg:h-[80%] animate-in fade-in duration-1000 delay-300">
         <img
           src="/images/bio.jpg"
           alt="Portrait"
